5cf046251593a48ebf96b498a5b4775266bdc112
adi-coder-02 <adityasinghal38@gmail.com>
2026-01-19 00:42:51 +0530

    feat: add BgChanger component for dynamic background color selection

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -4,15 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 import Counter from './components/Counter'
+import BgChanger from './components/BgChanger'
+
 
 function App() {
   
 
   return (
     <>
+      <BgChanger/>
       <Counter />
       <br />
       <Card username = "aditya singhal" btnText = "Read More" /> 
+      <br />
+      <br />
+
+      
+
     </>
   )
 }
---
+++ counterApp/src/components/BgChanger.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+
+function BgChanger() {
+  const [bgColor, setBgColor] = useState("white");
+  return (
+    <>
+    <div className="bg-container" style={{ backgroundColor: bgColor }}>
+      <h1 className="bg-heading">Background Color Changer</h1>
+      <div className='button'>
+        
+        <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('blue')}>
+          Blue
+        </button>
+
+        <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('green')}>
+          Green
+        </button>
+
+        <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('cyan')}>
+          Cyan
+        </button>
+
+        <button type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('teal')}>
+          Teal
+        </button>
+
+        <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('lime')}>
+          Lime
+        </button>
+
+        <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('red')}>
+          Red
+        </button>
+
+        <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('pink')}>
+          Pink
+        </button>
+      
+        <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => setBgColor('purple')}>
+          Purple
+        </button>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default BgChanger;
+
